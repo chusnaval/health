b7ab61bb57d8acbd88261bc069bd366edcb09881
chus <chusnaval@gmail.com>
2015-03-24 22:13:31 +0100

    fix combo view

--- user/views.py
+++ user/views.py
@@ -8,7 +8,7 @@ from models import User
 def search(request):
     query = request.GET.get('q', '')
     if query:
-        results = User.objects.get(login__icontains=query).order_by('id')
+        results = User.objects.filter(login__icontains=query)
         return render_to_response("search.html", {"results": results, "search": query})
     return render_to_response("search.html", {"results": [], "search": query})
 
@@ -19,7 +19,7 @@ def combo(request):
     query = request.GET.get('q', '')
     elements = User.objects.values('login').distinct()
     if query:
-        results = User.objects.filter(login=query)
+        results = User.objects.filter(login__icontains=query)
         return render_to_response("search_combo.html", {"results": results, "query": query, "elements": elements})
     return render_to_response("search_combo.html", {"results": [], "query": query, "elements": elements})
     #######################
