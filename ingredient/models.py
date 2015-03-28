6929be9be368a267eb46f2efa2bcc7898662c5aa
chus <chusnaval@gmail.com>
2015-03-29 00:19:59 +0100

    Adding new models

--- ingredient/models.py
+++ ingredient/models.py
@@ -3,10 +3,11 @@ from measurement.models import Measurement
 from recipe.models import Recipe
 
 
-
-
-
 class Ingredient(models.Model):
     name = models.CharField(max_length=50)
     measurement = models.OneToOneField(Measurement, primary_key=False)
     recipe = models.ForeignKey(Recipe)
+
+    def __str__(self):
+        return super(self.name)
+
